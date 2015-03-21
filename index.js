39737d08f91e0628d7240a8e973a76c665bdc3cb
Daniel Sellers <daniel@designfrontier.net>
2015-03-21 09:10:37 -0600

    filled out the jshintrc and have the get functionality finished mostly

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
-var http = requrie('http')
+var http = require('http')
+	, version = 'v1'
 	, key
 	, token
-	, get = function (id, callback, errorCallback) {
+	, get = function (idIn, callbackIn, errorCallbackIn) {
+		'use strict';
 
+		var id = idIn
+			, callback = callbackIn
+			, error = errorCallbackIn
+			, options = {
+				hostname: 'www.ansble.com',
+				port: 80,
+				path: '/api/' + version + '/' + key,
+				method: 'GET',
+				headers: {
+					'Authorization': token
+				}
+			};
+
+		if(typeof id === 'function'){
+			//getting everything for the app
+			callback = idIn;
+			error = callbackIn;
+		} else {
+			options.path = options.path + '/' + idIn;
+		}
+		
+		http.get(options, callback, error); //figure out scope here...
 	}
 	, save = function (item, callback, errorCallback) {
+		'use strict';
+
 
 	}
 
 	, setup = function (keyIn, tokenIn) {
+		'use strict';
+
 		key = keyIN;
 		token = tokenIn;
+
+		if(typeof keyIN !== 'string'){
+			throw 'key is required and must be a string';
+		}
+
+		if(typeof tokenIn !== 'string'){
+			throw 'token is required and must be a string';
+		}
 
 		return {
 			get: get
